f724c092e00af975bfadfe3bc6baf6ab9f49dd56
white_rabbit <dariagurova91@gmail.com>
2018-01-16 17:49:02 -0800

    Update lcm.c

--- lcm.c
+++ lcm.c
@@ -1,17 +1,17 @@
 unsigned int    lcm(unsigned int a, unsigned int b)
 {
-    unsigned int n1 = a, n2 = b, minMultiple;
+    unsigned int n1 = a, n2 = b, minM;
 
     if (n1 == 0 || n2 == 0)
         return (0);
-    minMultiple = (n1>n2) ? n1 : n2;
+    minM = (n1 > n2) ? n1 : n2;
     while(1)
     {
-        if( minMultiple%n1==0 && minMultiple%n2==0 )
+        if( min % n1 == 0 && min %n2 == 0 )
         {
             break;
         }
-        ++minMultiple;
+        ++min;
     }
-    return (minMultiple);
+    return (min);
 }
